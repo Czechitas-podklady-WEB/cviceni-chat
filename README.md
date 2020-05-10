12b1f31e89eb25814be7604ccc362205fe0e5d81
Filip Chalupa <chalupa.filip@gmail.com>
2020-05-10 19:38:59 +0200

    Basic steps

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Cvičení: Chat
+
+1. Prohlédni si soubor `index.html`. Stránka je nastylovaná pomocí [Bootstrapu](https://getbootstrap.com/). Nejsou potřeba žádné další CSS. Všimni si formulářových inputů `#name-input` a `#message-input`, elementu `#messages`, se kterými budeš později pracovat. Pro splnění zadání stačí upravovat pouze soubor `index.js`.
+1. Uprav soubor `index.js` tak, aby stránka zobrazovala nejnovější zprávy z api.
+1. Uprav soubor `index.js` tak, aby formulář pomocí api odesílal nové zprávy na server.
--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 'use strict'
 
-const SEND_MESSAGE_ENDPOINT =
-	'https://czechichat.herokuapp.com/api/send-message'
+/* ========== PŘIJÍMÁNÍ ZPRÁV ========== */
+
 const GET_MESSAGES_ENDPOINT =
 	'https://czechichat.herokuapp.com/api/list-messages'
+const messagesElement = document.querySelector('#messages')
 
+// @TODO: v opakovaném intervalu nahraj zprávy ze serveru a zobraz v elementu #messages
+
+/* ========== ODESÍLÁNÍ ZPRÁV ========== */
+
+const SEND_MESSAGE_ENDPOINT =
+	'https://czechichat.herokuapp.com/api/send-message'
 const nameInputElement = document.querySelector('#name-input')
 const messageInputElement = document.querySelector('#message-input')
 
-const messagesElement = document.querySelector('#messages')
-
 document.querySelector('#send-form').addEventListener('submit', (event) => {
-	/*
-	event.preventDefault()
+	event.preventDefault() // Zamezí přesměrování na jinou stránku
 
 	console.log('Name:', nameInputElement.value)
 	console.log('Message:', messageInputElement.value)
-	*/
-	// @TODO: odešli zprávu
-})
 
-// @TODO: v opakovaném intervalu nahraj zprávy ze serveru a zobraz v elementu #messages
+	// @TODO: odešli zprávu na server
+})
